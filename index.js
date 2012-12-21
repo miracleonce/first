f21ea1a0ea04d3e4df38adc690efbf94c630b304
xiaonuo <miracleonce@163.com>
2012-12-21 14:20:45 +0800

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: 元芳
+ * Date: 12-12-21
+ * Time: 下午2:09
+ * To change this template use File | Settings | File Templates.
+ */
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(80);
